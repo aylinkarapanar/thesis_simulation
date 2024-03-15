9fd056df87b7a5f4e7c882846b8bf06e20a3db6d
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-03-15 17:08:36 +0100

    Update thesis_03.15.R

--- thesis_03.15.R
+++ thesis_03.15.R
@@ -13,59 +13,59 @@ library(semTools)
 
 ##############
 #Measure (Week) Invariance
+results_df <- data.frame(
+  sample_size = integer(),
+  magnitude_level = numeric(),
+  chisq = numeric(),
+  pvalue = numeric(),
+  cfi = numeric(),
+  rmsea = numeric()
+)
 
 set.seed(123)
-n <- 100
+n <- c(50, 100, 250, 500)
+factor_loadings <- c(0.9, 0.8, 0.7, 0.6)
+iterations <- 1000
+for (size in n) {
+  for (loading in factor_loadings) {
+    for (iter in 1:iterations) {group1_parameters <- " latent =~ item1 + item2 + item3 + item4"
+    group2_parameters <- " latent =~ item1 + item2 + item3 + factor_loading*item4"
+    
+    metric_data_group1 <- simulateData(model = group1_parameters, sample.nobs = n)
+    metric_data_group2 <- simulateData(model = group2_parameters, sample.nobs = n)
+    
+    metric_data_group1$group <- "Group1"
+    metric_data_group2$group <- "Group2"
+    
+    metric_data <- rbind(metric_data_group1, metric_data_group2)
+    
+    model <- "latent =~ item1 + item2 + item3 + item4"
+    
+    cfa_metric <- cfa(model, data = metric_data , group = "group", group.equal = "loadings")
+    fit_measures <-fitMeasures(cfa_metric, c("chisq", "pvalue", "cfi", "rmsea"))
+    
+    temp_df <- data.frame(
+      sample_size = n*2,
+      magnitude_level = factor_loadings,
+      chisq = fit_measures['chisq'],
+      pvalue = fit_measures['pvalue'],
+      cfi = fit_measures['cfi'],
+      rmsea = fit_measures['rmsea']
+    )
+    
+    # Reset row names
+    row.names(temp_df) <- NULL
+    
+    # Bind the temporary dataframe to results_df
+    results_df <- rbind(results_df, temp_df)
+    }
+  }
+}
 
-group1_parameters <- " latent =~ item1 + item2 + item3 + item4"
-group2_parameters <- " latent =~ item1 + item2 + item3 + 0.5*item4"
-
-metric_data_group1 <- simulateData(model = group1_parameters, sample.nobs = n)
-
-metric_data_group2 <- simulateData(model = group2_parameters, sample.nobs = n)
-
-metric_data_group1$group <- "Group1"
-metric_data_group2$group <- "Group2"
-
-metric_data <- rbind(metric_data_group1, metric_data_group2)
-
-model <- "
-    latent =~ item1 + item2 + item3 + item4"
-
-metric_data <- rbind(metric_data_group1, metric_data_group2)
-
-cfa_metric <- cfa(model, data = metric_data , group = "group", group.equal = "loadings")
-
-summary(cfa_metric, fit.measures=T, standardized = T)
-#can be useful in comparing the fit indices 
-#config_vs_metric <- compareFit(cfa_config, cfa_metric)
-#summary(config_vs_metric)
-
-#
-set.seed(123)
-n <- 100
-
-group1_parameters <- " latent =~ item1 + item2 + item3 + item4"
-group2_parameters <- " latent =~ item1 + item2 + item3 + 0.5*item4"
-
-metric_data_group1 <- simulateData(model = group1_parameters, sample.nobs = n)
-metric_data_group2 <- simulateData(model = group2_parameters, sample.nobs = n)
-
-metric_data_group1$group <- "Group1"
-metric_data_group2$group <- "Group2"
-
-metric_data <- rbind(metric_data_group1, metric_data_group2)
-
-model <- "latent =~ item1 + item2 + item3 + item4"
-
-cfa_metric <- cfa(model, data = metric_data , group = "group", group.equal = "loadings")
-fit_measures <-fitMeasures(cfa_metric, c("chisq", "df", "pvalue", "cfi", "rmsea"))
-
-
-# Printing fit measures
-print(fit_measures)
+# Printing the data frame
+print(results_df)
 # Summary of the CFA
-#summary(cfa_metric, fit.measures = TRUE, standardized = TRUE)
+summary(cfa_metric, fit.measures = TRUE, standardized = TRUE)
 
 
 #Scalar (Strong) Invariance
