ddb0b76235a8736facb82eef09fc3fe9253758d6
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-03-25 16:02:02 +0100

    update

--- 03_25.R
+++ 03_25.R
@@ -13,16 +13,18 @@ create_model_string <- function(intercept, nitems) {
   intercept_part <- c()
   
   # Loop to generate intercepts for each subset of items
-  for (i in 1:nitems) {
-    current_intercepts <- rep(0, nitems)  # Initialize intercepts for the current subset
-    current_intercepts[(nitems - i + 1):nitems] <- intercept  # Set intercepts for the current subset
-    for (j in 1:nitems) {
-      intercept_part <- c(intercept_part, paste(item_names[j], "~", current_intercepts[j]))
-    }
+  for (i in 1:length(values)) {
+    output <- paste("latent =~ item1 + item2 + item3 + item4", "\n")
+    
+    # Add values stepwise to subsequent items
+    output <- paste(output, "item1 ~1*", c(0, 0, 0, values[i]), "\n")
+    output <- paste(output, "item2 ~1*", c(0, 0, values[i], values[i]), "\n")
+    output <- paste(output, "item3 ~1*", c(0, values[i], values[i], values[i]), "\n")
+    output <- paste(output, "item4 ~1*", c(values[i], values[i], values[i], values[i]), "\n")
   }
   
   
-  pop.model <- paste(pop.model, "\n", paste(intercept_part, collapse = "\n"))
+  pop.model <- paste(pop.model, "\n", paste(output, collapse = "\n"))
   return(pop.model)
 }
 
@@ -71,6 +73,48 @@ for (i in 1:length(values)) {
   cat(output, "\n")
 }
 
+#############################
+# Define sample sizes and intercept values
+n <- c(50, 100, 250, 500)
+intercepts <- c(0.9, 0.8, 0.7, 0.6)
+
+# Define the number of items
+nitems <- 4
+
+# Define a function to create the model string
+create_model_string <- function(intercepts, nitems) {
+  item_names <- paste0("item", 1:nitems)
+  pop.model <- paste0(item_names, collapse = "+" )
+  pop.model <- paste('latent =~ ', pop.model)
+  intercept_part <- character(length(intercepts))
+  
+  for (i in seq_along(intercepts)) {
+    output <- paste("latent =~", paste(item_names, collapse = " + "))
+    for (j in 1:nitems) {
+      intercept_values <- ifelse(1:nitems == j, intercepts[i], 0)
+      output <- paste(output, paste(item_names[j], "~ 1 * ", intercept_values[j]))
+    }
+    intercept_part[i] <- output
+  }
+  
+  return(intercept_part)
+}
+
+# Loop through sample sizes, intercepts, and iterations
+for (size in n) {
+  for (intercept in intercepts) {
+    # Define the model strings
+    model_strings <- create_model_string(intercepts = intercept, nitems = nitems)
+    
+    # Print the model strings to investigate
+    for (model_string in model_strings) {
+      print(model_string)
+    }
+    
+    # Your remaining code for simulation, CFA, fit measures, and storing results goes here
+    # ...
+  }
+}
 
 
 
