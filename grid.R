4ab858e97a7c6ba69c6f4c112e7f5320f961760d
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-03-26 08:48:05 +0100

    Update grid.R

--- grid.R
+++ grid.R
@@ -7,16 +7,40 @@ intercepts <- c(0.9, 0.8, 0.7, 0.6)
 models <- c()
 
 
+#for (i in 1:length(intercepts)) {
+  #output <- paste("latent =~ item1 + item2 + item3 + item4", "\n")
+  
+  #output <- paste(output, "item1 ~", c(0, 0, 0, intercepts[i]), "*1","\n")
+  #output <- paste(output, "item2 ~", c(0, 0, intercepts[i], intercepts[i]), "*1", "\n")
+  #output <- paste(output, "item3 ~", c(0, intercepts[i], intercepts[i], intercepts[i]),"*1", "\n")
+  #output <- paste(output, "item4 ~", c(intercepts[i], intercepts[i], intercepts[i], intercepts[i]),"*1", "\n")
+  
+  #models <- c(models, output)
+#}
+
+#for (intercept in intercepts) {
+#  output <- paste("latent =~ item1 + item2 + item3 + item4", "\n")
+  
+#  for (i in 1:length(intercepts)) {
+#    intercept_vector <- c(rep(0, i - 1), intercept, rep(intercept, 4 - i))
+#    output <- paste(output, paste("item", i, "~", intercept_vector, "*1", "\n"))
+#  }
+#  models <- c(models, output)
+#}
+
 for (i in 1:length(intercepts)) {
+  intercept <- intercepts[i]
   output <- paste("latent =~ item1 + item2 + item3 + item4", "\n")
   
-  output <- paste(output, "item1 ~", c(0, 0, 0, intercepts[i]), "*1","\n")
-  output <- paste(output, "item2 ~", c(0, 0, intercepts[i], intercepts[i]), "*1", "\n")
-  output <- paste(output, "item3 ~", c(0, intercepts[i], intercepts[i], intercepts[i]),"*1", "\n")
-  output <- paste(output, "item4 ~", c(intercepts[i], intercepts[i], intercepts[i], intercepts[i]),"*1", "\n")
+  intercept_vector <- c(rep(0, i - 1), intercept, rep(intercept, 4 - i))
   
-  models <- c(models, output)
+  for (j in 1:length(intercepts)) {
+    output <- paste(output, paste("item", j, "~", intercept_vector[j], "*1", "\n"))
+  }
+  
+  models[i] <- output
 }
+
 
 design <- expand.grid(model = models, sample_size = n)
 
@@ -25,6 +49,7 @@ magnitude_level <- rep(intercepts, each = 4, times = 4)
 design$magnitude_level <- magnitude_level
 design$ndesign <- 1:nrow(design)
 
+#where to put set seed?
 set.seed(123)
 
 results <- expand.grid(ndesign = design$ndesign,
