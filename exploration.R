a92932fccde0bdbc4b16b50e109ce4f694391828
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-04-07 16:30:06 +0200

    Update exploration.R

--- exploration.R
+++ exploration.R
@@ -5,11 +5,11 @@ names(mean_chisq) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group 
 sd_chisq <- aggregate(results$chisq, by = list(results$model_ratios, results$magnitude_level, results$group_size), FUN = sd)
 names(sd_chisq) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "SD Chi-square")
 
-# calculating the percentage of p-values for chi-square that are higher than 0.05 for each condition
+# calculating the ratio of p-values for chi-square that are higher than 0.05 for each condition
 satisfactory_chisq <- aggregate(ifelse(results$pvalue > 0.05, 1, 0), 
                                 by = list(results$model_ratios, results$magnitude_level, results$group_size), 
                                 FUN = mean)
-names(satisfactory_chisq) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory % Chi-square")
+names(satisfactory_chisq) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory Chi-square Ratio")
 
 # calculating the mean and standard deviation of RMSEA for each condition 
 mean_rmsea <- aggregate(results$rmsea, by = list(results$model_ratios, results$magnitude_level, results$group_size), FUN = mean)
@@ -23,7 +23,7 @@ satisfactory_rmsea <- aggregate(interpret_rmsea(results$rmsea, rules = 'byrne199
                                            by = list(results$model_ratios, results$magnitude_level, results$group_size), 
                                            FUN = function(x) mean(x == "satisfactory"))
 
-names(satisfactory_rmsea) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory % RMSEA")
+names(satisfactory_rmsea) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory RMSEA Ratio")
 
 
 # calculating the mean and standard deviation of CFI for each condition 
@@ -38,7 +38,7 @@ satisfactory_cfi <- aggregate(interpret_cfi(results$cfi, rules = "hu&bentler1999
                                          by = list(results$model_ratios, results$magnitude_level, results$group_size), 
                                          FUN = function(x) mean(x == "satisfactory"))
 
-names(satisfactory_cfi) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory % CFI")
+names(satisfactory_cfi) <- c("Noninvariance Ratios", "Intercept Value", "Sample Group Size", "Satisfactory CFI Ratio")
 
 
 # merge mean, standard deviation, and percentages
