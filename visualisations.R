fd1b6c03766c7bb94ab8e9e2f7143dc7bcb5388b
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-04-18 10:11:17 +0200

    Update visualisations.R

--- visualisations.R
+++ visualisations.R
@@ -1,59 +1,17 @@
 #install.packages('ggplot2')
 #install.packages('gridExtra')
+#install.packages('dplyr')
+#install.packages('jtools')
 library(ggplot2)
 library(gridExtra)
-
+library(dplyr)
+library(jtools)
 
 directory <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
 custom_colors <- c("0.1" = "#1b9e77", "0.2" = "#d95f02", "0.3" = "#7570b3", "0.4" = "#e7298a")
 
-chisq_color_plot <- ggplot(results, aes(x = group_size, y = chisq, color = magnitude_level)) +
-  geom_boxplot() +
-  theme_minimal() +
-  facet_wrap(~model_ratios) +
-  scale_color_manual(name = "Magnitude Level", values = custom_colors) +
-  labs(title = "Boxplot of Chi Square Values",
-       subtitle = "by Sample Group Size, Magnitude Level & Noninvariance Ratios", 
-       x = "Sample Group Size",
-       y = "Chi-Square") 
 
-chisq_file_path <- file.path(directory, "chisq_color_plot.png")
-ggsave(chisq_file_path, plot = chisq_color_plot, width = 12, height = 6)
-
-
-rmsea_color_plot <- ggplot(results, aes(x = group_size, y = rmsea, color = magnitude_level)) +
-  geom_boxplot() +
-  theme_minimal() +
-  facet_wrap(~model_ratios) +
-  scale_color_manual(name = "Magnitude Level", values = custom_colors) +
-  labs(title = "Boxplot of RMSEA Values",
-       subtitle = "by Sample Group Size, Magnitude Level & Noninvariance Ratios",
-       x = "Sample Group Size",
-       y = "RMSEA")
-file_path <- file.path(directory, "rmsea_color_plot.jpeg")
-ggsave(file_path, rmsea_color_plot, width = 12, height = 6)
-
-
-cfi_color_plot <- ggplot(results, aes(x = group_size, y = cfi, color = magnitude_level)) +
-  geom_boxplot() +
-  theme_minimal() +
-  facet_wrap(~model_ratios) +
-  scale_color_manual(name = "Magnitude Level", values = custom_colors) +
-  labs(title = "Boxplot of CFI Values",
-       subtitle = "by Sample Group Size, Magnitude Level & Noninvariance Ratios", 
-       x = "Sample Group Size",
-       y = "CFI") 
-
-cfi_file_path <- file.path(directory, "cfi_color_plot.png")
-ggsave(cfi_file_path, plot = cfi_color_plot, width = 12, height = 6)
-
-#combined_plots <- grid.arrange(chisq_color_plot, rmsea_color_plot, cfi_color_plot, nrow = 3)
-#combined_file_path <- file.path(directory, "combined_plots.jpeg")
-#ggsave(combined_file_path, plot = combined_plots, width = 12, height = 18)
-
-#install.packages('dplyr')
-library(dplyr)
 
 #ADD CUT OFFS AS LINES
 
@@ -68,7 +26,7 @@ mean_chisq <- results %>%
 chisq_line_plot <- ggplot(mean_chisq, aes(x = group_size, y = mean, color = magnitude_level)) +
   geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0, position = position_dodge(width = 0.5)) + 
   geom_point(position = position_dodge(width = 0.5), size = 1.5) +  
-  theme_minimal() +
+  theme_apa() +
   facet_wrap(~model_ratios) +
   scale_color_manual(name = "Magnitude Level", values = custom_colors) +
   labs(title = "Graph of the Mean of Chi-Square Values with 95% Confidence Interval",
@@ -91,7 +49,7 @@ mean_rmsea <- results %>%
 rmsea_line_plot <- ggplot(mean_rmsea, aes(x = group_size, y = mean, color = magnitude_level)) +
   geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0, position = position_dodge(width = 0.5)) +  
   geom_point(position = position_dodge(width = 0.5), size = 1.5) +  
-  theme_minimal() +
+  theme_apa() +
   facet_wrap(~model_ratios) +
   scale_color_manual(name = "Magnitude Level", values = custom_colors) +
   labs(title = "Graph of the Mean of RMSEA Values with 95% Confidence Interval",
@@ -115,7 +73,7 @@ mean_cfi <- results %>%
 cfi_line_plot <- ggplot(mean_cfi, aes(x = group_size, y = mean, color = magnitude_level)) +
   geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0, position = position_dodge(width = 0.5)) +  
   geom_point(position = position_dodge(width = 0.5), size = 1.5) +  
-  theme_minimal() +
+  theme_apa() +
   facet_wrap(~model_ratios) +
   scale_color_manual(name = "Magnitude Level", values = custom_colors) +
   labs(title = "Graph of the Mean of CFI Values with 95% Confidence Interval",
