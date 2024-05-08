e5d2518e6e31f0d358c5672226d19aeb81477959
aylinkarapanar <159909836+aylinkarapanar@users.noreply.github.com>
2024-05-08 16:21:52 +0200

    Update visualisations.R

--- visualisations.R
+++ visualisations.R
@@ -116,3 +116,47 @@ cfi_file_path <- file.path(directory, "cfi_line_plot.png")
 ggsave(cfi_file_path, plot = cfi_line_plot, width = 12, height = 6)
 
 
+#############
+mean_rmsea <- results %>%
+  group_by(group_size) %>%
+  summarise(mean = mean(rmsea),
+            se = sd(rmsea) / sqrt(n()),
+            ci_low = mean - 1.96 * se,
+            ci_high = mean + 1.96 * se)
+
+# Plot for main effect of sample size
+sample_size_plot <- ggplot(mean_rmsea, aes(x = group_size, y = mean)) +
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0, position = position_dodge(width = 0.5)) +  
+  geom_point(position = position_dodge(width = 0.5), size = 1.5) +  
+  theme_apa() +
+  scale_color_manual(name = "Magnitude Level", values = custom_colors) +
+  labs(title = "Graph of the Mean of RMSEA with 95% Confidence Interval by Sample Size Per Group",
+       subtitle = NULL, 
+       x = "Sample Size Per Group",
+       y = "Mean RMSEA") +
+  theme(legend.position = "bottom") +
+  geom_segment(aes(x = 0, xend = 2.5, y = 0.010, yend = 0.010), linetype = "dotted", color = "black") +
+  geom_segment(aes(x = 2.5, xend = 5, y = 0.015, yend = 0.015), linetype = "dotted", color = "black") +
+  scale_y_continuous(breaks = c(0.0, 0.010, 0.015, 0.02, 0.04, 0.06))
+
+# Plot for main effect of magnitude level
+mean_rmsea <- results %>%
+  group_by(magnitude_level) %>%
+  summarise(mean = mean(rmsea),
+            se = sd(rmsea) / sqrt(n()),
+            ci_low = mean - 1.96 * se,
+            ci_high = mean + 1.96 * se)
+magnitude_level_plot <- ggplot(mean_rmsea, aes(x = magnitude_level, y = mean)) +
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.5, position = position_dodge(width = 0.5)) +  
+  geom_point(position = position_dodge(width = 0.5), size = 1.5) +  
+  theme_apa() +
+  labs(title = "Graph of the Mean of RMSEA with 95% Confidence Interval by Magnitude Level",
+       subtitle = NULL, 
+       x = "Magnitude Level",
+       y = "Mean RMSEA") +
+  theme(legend.position = "bottom")
+
+# Print the plots
+print(sample_size_plot)
+print(magnitude_level_plot)
+
